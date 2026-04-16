eba3a1ac699af6c2305edb3bff47f49baee62421
krithickm10-afk <krithickm10@gmail.com>
2026-04-16 16:43:40 +0530

    Add files via upload

---
+++ a1.py
@@ -0,0 +1,12 @@
+# printing a greeting message
+print ("Hello, world")
+
+# printing my name 
+print ("My name is Alex.")
+
+# printing a fun fact about myself
+# print("I love playing football")
+print("I love playing playing football")
+
+# performing and printing a simple caculation
+print("The result of 3 + 5 is:", 3 + 5)
