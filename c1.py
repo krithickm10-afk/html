786271ae1bca207c10b1e26fc368ff068aa381f4
krithickm10-afk <krithickm10@gmail.com>
2026-04-21 16:37:45 +0530

    Add files via upload

---
+++ c1.py
@@ -0,0 +1,13 @@
+#Creating different variables 
+x = 5
+y = "John"
+print(x)
+print(y)
+
+codingal = "codingal"
+print(codingal)
+
+# Uaer Input
+name = input("enter your name: ")
+
+print("\nHello", name, "\nwelcome to codingal")
