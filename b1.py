3b58887579ee00283dcb1ceb666c3fde5af088e4
krithickm10-afk <krithickm10@gmail.com>
2026-04-21 16:29:22 +0530

    Add files via upload

---
+++ b1.py
@@ -0,0 +1,14 @@
+#Print a Sentance 
+print('Welcome to the world of programming!')
+
+#Print a Number 
+print(4)
+
+#Next line after print 
+print ("Hello Python \n")
+
+#Pass multiple arguments for printing 
+print("hello",5)
+
+#End argument of Print Statement
+print("Welcome to ", end= "*")
