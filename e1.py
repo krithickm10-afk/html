81b45c0403694c88cc9af631e1a3c19b59edb05f
krithickm10-afk <krithickm10@gmail.com>
2026-04-23 16:33:47 +0530

    Add files via upload

---
+++ e1.py
@@ -0,0 +1,27 @@
+# Assigning Different Variables
+name = "Penguin"
+age = 15
+is_student = True
+weight = 38.5
+
+# Printing Different Variables and their Data Type
+print("Name :", name)
+print("Data Type of Name is", type(name))
+
+print("Age :", age)
+print("Data type of Age is", type(age))
+
+print("is_student :", is_student)
+print("Data type of is_student is", type(is_student))
+
+print("Weight :", weight)
+print("Data type of Weight is", type(weight))
+
+#Type casting to convert the datatypeof variables
+print("\n After Type Casting....")
+age = str(age)
+print(age)
+print("Data type of age is", type(age))
+weight = int(weight)
+print(weight)
+print("Data Type of Weight is", type(weight))
