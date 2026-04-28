783574d4871e777199571dd590ee7673e3825a18
krithickm10-afk <krithickm10@gmail.com>
2026-04-28 16:36:38 +0530

    Add files via upload

---
+++ l.py
@@ -0,0 +1,8 @@
+i = int(input("enter a number : "))
+if (i < 15):
+    print ("i is smaller than 15")
+    print ("i am in if Block")
+else:
+    print ("i is greater than 15")
+    print ("i am in else Block")
+print ("i am not in if and not in else Block")
