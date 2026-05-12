2e5ba0ce5e78bf45489f2e5861e6f9ada32b4035
krithickm10-afk <krithickm10@gmail.com>
2026-05-12 15:01:58 +0530

    Add files via upload

---
+++ home1.py
@@ -0,0 +1,13 @@
+print("Birthdays of 5 friends\n")
+
+# Storing Birthdays in variables and printing them
+a="13/02/2014"
+print("akshath -",a)
+b="14/04/2014"
+print("\nsunny -",b)
+c="23/07/2014"
+print("\nratissh -",c)
+d="27/13/2014"
+print("\nkrithick -",d)
+e="14/04/2014"
+print("\nsiddharth -",e)
