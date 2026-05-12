9d96143b56077ee7edf1485714f6b7f037080527
krithickm10-afk <krithickm10@gmail.com>
2026-05-12 15:00:11 +0530

    Add files via upload

---
+++ home3.py
@@ -0,0 +1,9 @@
+num = 49
+num_sqrt = num**0.5
+print(f'The Number is {num} and it is stored in num_sqrt{num_sqrt}')
+# Python Program to calculate the square root
+
+num = int(input("enter the number:"))
+
+num_sqrt = num**0.5
+print('The square root of %0.2f is %0.3f' % (num, num_sqrt))
