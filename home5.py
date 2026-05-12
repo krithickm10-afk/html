64d4f50a92bbed81316b37f72742c587547763c7
krithickm10-afk <krithickm10@gmail.com>
2026-05-12 14:55:31 +0530

    Add files via upload

---
+++ home5.py
@@ -0,0 +1,8 @@
+x = input("Enter the ASCII Code between 65-90: ")
+
+if x == "65":
+    print("The ASCII code 65 stands for Alphabet A")
+elif x == "66":
+    print("The ASCII code 66 stands for Alphabet B")
+elif x == "67":
+     print("The ASCII code 66 stands for Alphabet c")
