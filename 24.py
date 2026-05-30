95c081d5b9142b8d59fb648fb2041c1a33037490
krithickm10-afk <krithickm10@gmail.com>
2026-05-30 12:00:15 +0530

    Add files via upload

---
+++ 24.py
@@ -0,0 +1,8 @@
+var = 10            #initialise
+while var > 0:      #iterate loop
+   var = var - 1
+   if var == 5:       #condition 1
+      continue        #condition statement
+      #display result
+   print ('\nCurrent variable value :', var)
+print ("\nGood bye!")
