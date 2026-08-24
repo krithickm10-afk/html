dd1216726b160a37050cb036498395d9eadf5245
krithickm10-afk <krithickm10@gmail.com>
2026-08-24 17:16:05 +0530

    Add files via upload

--- 1.py
+++ 1.py
@@ -1,8 +1,18 @@
-#input an integer value 
-n = int(input("Enter the number whose sum you want to find:"))
-sum=0
+# Take input from the user
+number = int(input("Enter your number: "))
 
-#Iterates for n+1 times: i=1 to n+1
-for i in range(1, n+1):
-    sum = sum+i
-    print("\nSum =", sum)
+# Store the original number for comparison later
+original_number = number
+reversed_number = 0
+
+# Reverse the number 
+while number > 0:
+    digit = number % 10 
+    reversed_number = reversed_number * 10 + digit
+    number //= 10
+
+# Check if the original number and the reversed number are the same
+if original_number == reversed_number:
+    print(f"{original_number} is a palindrome")
+else:   
+    print(f"{original_number} is not a palindrome") 
