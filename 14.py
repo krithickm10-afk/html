4c7427655014b3b8baa5a6fa9ba6a2b0850afe62
krithickm10-afk <krithickm10@gmail.com>
2026-09-03 17:38:12 +0530

    Add files via upload

--- 14.py
+++ 14.py
@@ -1,30 +1,14 @@
-import turtle
+# 03-two-odd-split.py
+# Topic: XOR of Two Odd-occuring Numbers, Splitting by the Rightmost Set Bit
 
-turtle.Screen().bgcolor("Aqua")
-board = turtle.Turtle()
+input("XOR all - pairs cancel, two odd-occuring ones remain.  Press Enter ")
+print("  [1, 4, 3, 3]  XOR of all:", 1^4^3^3, "  binary:", bin(1^4^3^3)[2:])
+print("  split bit 1  -> group A (bit 0 ON): 1  group B (bit 0 OFF): 4")
 
-# first triangle for star
-board.forward(100) # draw base
-
-board.left(120)
-board.forward(100)
-
-board.left(120)
-board.forward(100)
-
-board.penup()
-board.right(150)
-board.forward(50)
-
-# second triangle for star
-board.pendown()
-board.right(90)
-board.forward(100)
-
-board.right(120)
-board.forward(100)
-
-board.right(120)
-board.forward(100)
-
-turtle.done()
+n = int(input("Enter a number (try 6 or 9): "))
+guess = input("Is bit 0 of " + str(n) +  " ON? (ye/no): ")
+input("Check the split bit.  Press Enter ")
+if n & 1:
+    print(" ", n, "  binary:", bin(n)[2:], " bit 0 ON - group A your guess:", guess)
+else:
+    print(" ", n, "  binary:", bin(n)[2:], "  bit 0 OFF - group B your guess:", guess)
