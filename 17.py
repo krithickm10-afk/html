b7be7a4bf93e4ce2900febbaf0dc46c7adbf8022
krithickm10-afk <krithickm10@gmail.com>
2026-09-07 17:29:00 +0530

    Add files via upload

--- 17.py
+++ 17.py
@@ -1,7 +1,11 @@
-def weather_condition():
-    print('The weather is pleasent in:', spring)
-    print('The weather is same in', autumn)
+# 03-sign-detect.py
+# Topic: XOR for Sign Detection
 
-spring ="autumn"
-autumn ="spring"
-weather_condition()
+input("XOR sign detection - n ^ m < 0 means different signs.  Press Enter ")
+print("  4 ^ 2 =", 4 ^ 2, "  same signs  positive")
+print("  4 ^ -2 =", 4 ^ -2, "  different signs  negative")
+
+n = int(input("Enter a number (try 5 or -3): "))
+guess = input("Will " + str(n) + " ^ -8 be positive or negative? ")
+input("XOR is neagtive when signs differ. Press Enter ")
+print(" ", n, "^ -8 =", n ^ -8, "  your guess:", guess)
