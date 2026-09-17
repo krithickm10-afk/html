a236175668c2abf6176f10b794f93b5ad1ee67c6
krithickm10-afk <krithickm10@gmail.com>
2026-09-17 17:28:28 +0530

    Add files via upload

--- 26.py
+++ 26.py
@@ -1,20 +1,18 @@
-try:
-    num1, num2 = eval(input("Enter two numbers, seperated by a comma : "))
-    result = num1 / num2
-    print("Result is", result)
-#using multiple except block for different type of error
+# 03-list-max.py
+# Topic: Largest Element with Recursion
 
-except  ZeroDivisionError:
-    print("Division by zero is error !!")
+def list_max(lst):
+    if len(lst) == 1:
+        return lst[0]
+    rest = list_max(lst[1:])
+    return lst[0] if lst[0] > rest else rest
 
-except SyntaxError:
-    print("Comma is missing. Enter numbers separated ny comma like this 1, 2")
+input("Recursive max - compare head to max of tail.  Press Enter ")
+print("  list_max([3, 7, 2]) =", list_max([3, 7, 2]))
+print("  list_max([8, 1, 5]) =", list_max([8, 1, 5]))
 
-except:
-    print("Wrong input")
-
-else:
-    print("No exceptions")
-
-finally:
-    print("This will execute no matter what")
+n = int(input("Enter a number (try 6 or 15): "))    
+lst = [n, 4, 9, 2]
+guess = input("What is the largest in " + str(lst) + "? ")
+input("list_max compares head to max of tail at each step.  Press Enter ")
+print("  largest:", list_max(lst), "  your guess:", guess)
